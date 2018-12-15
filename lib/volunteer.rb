72ba2f71f83eb9d88fd6f581bfbcac3d51afcd84
Student <example@example.com>
2018-12-14 16:16:35 -0800

    write method self.find to find volunteer by their ID

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -9,22 +9,32 @@ class Volunteer
   end
 
   def ==(another_volunteer)
-   self.name().==(another_volunteer.name())
+    self.name().==(another_volunteer.name())
   end
 
   def save
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+
   end
 
   def self.all
-    volunteers = DB.exec("SELECT * FROM volunteers;")
-    many_volunteers = []
-    volunteers.each() do |volunteer|
+    volunteers = []
+    result = DB.exec("SELECT * FROM volunteers;")
+    result.each() do |volunteer|
       name = volunteer.fetch("name")
-      project_id = volunteer.fetch("project_id").to_i()
-      id = volunteer.fetch("id").to_i()
-      many_volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
-    many_volunteers
+    volunteers
   end
+
+  def self.find(id)
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = id;").first
+    name = volunteer.fetch("name")
+    project_id = volunteer.fetch("project_id").to_i
+    id = volunteer.fetch("id").to_i
+    Volunteer.new({:name => name, :id => id, :project_id => project_id})
+  end
+
 end
