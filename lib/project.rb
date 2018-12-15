9b84c2e92f5c0301245bacaeb6594dcfb8ad1416
Student <example@example.com>
2018-12-14 18:05:25 -0800

    spec6 in class Project- fail

--- lib/project.rb
+++ lib/project.rb
@@ -7,24 +7,25 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def ==(another_project)
+    self.title().==(another_project.title())
+  end
+  
   def save
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first.fetch("id").to_i
   end
 
-  def ==(another_project)
-    self.title().==(another_project.title())
-  end
 
   def self.all
     projects = []
-    # result = DB.exec("SELECT * FROM projects;")
-    # result.each do |project|
-    #   title = project.fetch("title")
-    #   id = project.fetch("id").to_i
-    #   projects.push(Project.new(:title => title, :id => id))
-    # end
-    # projects
+    result = DB.exec("SELECT * FROM projects;")
+    result.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new(:title => title, :id => id))
+    end
+    projects
   end
 
 
