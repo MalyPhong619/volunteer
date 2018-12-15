51135b3bd19305ad738aa40b664be273b5c2d95b
Student <example@example.com>
2018-12-14 16:51:31 -0800

    pass spec3 - create table in db - saves into db and returns id

--- lib/project.rb
+++ lib/project.rb
@@ -2,12 +2,15 @@ class Project
 
   attr_accessor :title, :id
 
-    def initialize(attributes)
-      @title = attributes.fetch(:title)
-      @id = attributes.fetch(:id)
-    end
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
 
-
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
 
 
 
