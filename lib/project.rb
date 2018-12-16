ce917c73fcadfeb6bbb704912a428f78dc969e2f
Student <example@example.com>
2018-12-16 15:44:05 -0800

    create volunteers method and pass spec 9 for Project

--- lib/project.rb
+++ lib/project.rb
@@ -35,5 +35,17 @@ class Project
     Project.new({:title => title, :id => id})
   end
 
+  def volunteers
+    volunteers = []
+    result = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id};")
+    result.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
 
 end
